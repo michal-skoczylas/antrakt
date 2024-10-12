68416c6227235c34baa83e5116dc21983ccc1aac
michal-skoczylas <privmichalskoczylas@gmail.com>
2024-10-12 23:05:21 +0200

    Dodanie palety kolorow

---
+++ colors.js
@@ -0,0 +1,35 @@
+// colors.js
+
+/* Primary: Monstera Deliciosa */
+var primary100 = "#F7FFF2";
+var primary200 = "#D4FEB9";
+var primary300 = "#B5F87D";
+var primary400 = "#96E842";
+var primary500 = "#77C90C";
+var primary600 = "#66A104";
+var primary700 = "#527801";
+var primary800 = "#3B4F00";
+var primary900 = "#1F2600";
+
+/* Accent: Bloodthirsty Lips */
+var accent100 = "#FFF2F2";
+var accent200 = "#FEB9B9";
+var accent300 = "#F87E7E";
+var accent400 = "#E64344";
+var accent500 = "#C40E1A";
+var accent600 = "#9D0518";
+var accent700 = "#750116";
+var accent800 = "#4E0013";
+var accent900 = "#26000B";
+
+/* Neutral */
+var neutral100 = "#FBFCFA";
+var neutral200 = "#EBEEE8";
+var neutral300 = "#DBE0D6";
+var neutral400 = "#CCD2C5";
+var neutral500 = "#BEC4B5";
+var neutral600 = "#989D8F";
+var neutral700 = "#72756B";
+var neutral800 = "#4C4E46";
+var neutral900 = "#262622";
+
